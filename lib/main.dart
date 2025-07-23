9110b1d56ba1b0e1498de914c6365f7f67ea3e2f
Sara Bouzit <bouz0032@algonquinlive.com>
2025-07-23 14:12:50 -0400

    Removed More Comments

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Increment nuber',
         child: const Icon(Icons.add),
       ),
     );
